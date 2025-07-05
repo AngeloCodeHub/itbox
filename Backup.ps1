38f978e600f3efd0681ba4582dc170ecb0293578
Angelo <a0920217700@gmail.com>
2025-07-05 16:29:44 +0800

    修正 Backup.ps1 中的備份路徑，移除不必要的 "Confidential\" 字串，確保備份目的地正確

--- Backup.ps1
+++ Backup.ps1
@@ -4,7 +4,7 @@ $puttyRegBackupPath = "Confidential\putty-settings.reg"
 reg export $puttyRegPath $puttyRegBackupPath /y
 
 $env = (Get-Content -Path "Confidential\env.json") | ConvertFrom-Json
-$BackupDst = $env[0].BackupDst + "Confidential\" `
+$BackupDst = $env[0].BackupDst `
   + (Get-Date -Format "ITBox-yyyyMMddHH") + ".zip"
 # Write-Host $BackupDst
 Compress-Archive -Path ".\Confidential" -DestinationPath $BackupDst -Force
